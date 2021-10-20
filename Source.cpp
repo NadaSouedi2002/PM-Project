2dd5db2bce87ea41f0165d88a8041b72d991f742
NadaSoudi2002 <nodyrony2002@gmail.com>
2021-10-20 13:23:57 +0200

    first code

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, z;
+	cout << "enter x" << endl;
+	cin >> x;
+	cout << "enter z" << endl;
+	cin >> z;
+	cout << "x + z =" << " " << x + z << endl;
+	system("pause");
+	return 0;
+}
